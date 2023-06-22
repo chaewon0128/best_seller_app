ecfbca21473f07e4455947f0b1c0ed0beed6cf7a
chaewon <juh7814@gmail.com>
2023-06-22 10:32:11 +0900

    💄 about 페이지 완료

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+
+export default function About() {
+    return (
+        <main>
+            <div className="content">
+            <h2>About Us</h2>
+            <p>Welcome to the official explorer for The New York Times Best Seller list explorer. <br />
+            We hope you enjoy your stay!</p> 
+            </div>
+
+            <style jsx>{`
+                .content {
+                       max-width: 700px; 
+                       margin: 0 auto;
+                       padding: 40px 30px;
+                       border: 2px solid #bdc3c7;
+                       border-top: none;
+                       box-shadow: 5px 5px 30px #bdc3c7;
+                }
+                h2 {
+                    font-size: 60px;
+                    margin-bottom: 20px;
+                }
+                p {
+                    font-size: 16px;
+                    line-height: 40px;
+          
+                }
+                `}
+
+            </style>
+        </main>
+    );
+}
+
