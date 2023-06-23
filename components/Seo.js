edbb25e51d919cc51bba226ba3771c3409db6b81
chaewon <juh7814@gmail.com>
2023-06-23 13:35:56 +0900

    🔇 console.log 코드 삭제

--- components/Seo.js
+++ components/Seo.js
@@ -5,7 +5,6 @@ import { useRouter } from "next/router";
 const obj = { '/': 'Home', '/about': 'About', '/list/[...params]' :  'Detail'};
 export default function Seo() {
     const router = useRouter()
-    console.log(router)
     return (
         <Head>
             <title>{obj[router.pathname]}</title>
